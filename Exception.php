12796baff8f791f5cc6973fe9973e120e54dd905
Evan D'Elia <deliaevan1@gmail.com>
2015-07-27 12:11:49 -0400

    Create Exception.php
    Credit goes to Martin Bean for helping with the original Dribbble API

---
+++ Exception.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * PHP wrapper for the Dribbble API.
+ * 
+ * @author   Martin Bean <martin@martinbean.co.uk>
+ * @license  MIT License
+ * @version  3.0
+ */
+
+
+/**
+ * Thrown when an API call returns an exception.
+ */
+class Exception extends \Exception
+{
+}
